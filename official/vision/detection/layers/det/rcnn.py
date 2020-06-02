6c2e3e55f9a8d10f5884d55e79401f4a2e52c00b
王枫（实习） <wangfeng02@megvii.com>
2020-06-02 11:22:22 +0800

    add License header

--- official/vision/detection/layers/det/rcnn.py
+++ official/vision/detection/layers/det/rcnn.py
@@ -1,6 +1,11 @@
-#!/usr/bin/python3
 # -*- coding:utf-8 -*-
-
+# MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
+#
+# Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 import megengine as mge
 import megengine.functional as F
 import megengine.module as M
