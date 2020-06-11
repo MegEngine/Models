66387c59c039e3075ee7eb447153e2c3a507e2cc
luozhengxiong <luozhengxiong@megvii.com>
2020-06-11 18:43:48 +0800

    update init

--- official/vision/keypoints/models/__init__.py
+++ official/vision/keypoints/models/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> format code
 # -*- coding: utf-8 -*-
 # MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
 #
@@ -11,34 +7,9 @@
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 from .simplebaseline import (
-<<<<<<< HEAD
-<<<<<<< HEAD
     simplebaseline_res50,
     simplebaseline_res101,
     simplebaseline_res152,
 )
-<<<<<<< HEAD
-=======
-from .simplebaseline import SimpleBaseline_Res50, SimpleBaseline_Res101, SimpleBaseline_Res152 
->>>>>>> fix hubconf
-=======
-    SimpleBaseline_Res50,
-    SimpleBaseline_Res101,
-    SimpleBaseline_Res152,
-=======
-    simplebaseline_res50,
-    simplebaseline_res101,
-    simplebaseline_res152,
->>>>>>> add comments
-)
->>>>>>> format code
-=======
 
-<<<<<<< HEAD
-from .mspn import (
-    mspn_4stage,
-)
->>>>>>> add mspn
-=======
 from .mspn import mspn_4stage
->>>>>>> format code
