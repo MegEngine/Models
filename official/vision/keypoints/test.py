79c5fd00e1d91f180e02a126c0665bcf915b5b83
luozhengxiong <luozhengxiong@megvii.com>
2020-06-20 02:24:46 +0800

    update

--- official/vision/keypoints/test.py
+++ official/vision/keypoints/test.py
@@ -34,11 +34,7 @@ logger = mge.get_logger(__name__)
 
 def build_dataloader(rank, world_size, data_root, ann_file):
     val_dataset = COCOJoints(
-<<<<<<< HEAD
         data_root, ann_file, image_set="val2017", order=("image", "boxes", "info")
-=======
-        data_root, ann_file, image_set="val", order=("image", "boxes", "info")
->>>>>>> add readme
     )
     val_sampler = SequentialSampler(val_dataset, 1, world_size=world_size, rank=rank)
     val_dataloader = DataLoader(
@@ -237,11 +233,7 @@ def make_parser():
             "simplebaseline_res50",
             "Simplebaseline_res101",
             "Simplebaseline_res152",
-<<<<<<< HEAD
             "mspn_4stage",
-=======
-            "mspn_4stage"
->>>>>>> add readme
         ],
     )
     parser.add_argument(
@@ -260,7 +252,6 @@ def main():
     parser = make_parser()
     args = parser.parse_args()
 
-<<<<<<< HEAD
     dt_path = os.path.join(cfg.data_root, "person_detection_results", args.dt_file)
     dets = json.load(open(dt_path, "r"))
 
@@ -268,10 +259,6 @@ def main():
         cfg.data_root, "annotations", "person_keypoints_val2017.json"
     )
     eval_gt = COCO(gt_path)
-=======
-    dets = json.load(open(args.dt_path, "r"))
-    eval_gt = COCO(args.gt_path)
->>>>>>> add readme
     gt = eval_gt.dataset
 
     dets = [
